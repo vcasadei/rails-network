2dbaca75e8e587b93ff3f14a37e7543af43f9d8a
Vitor Casadei <vitor.casadei@gmail.com>
2012-12-27 11:04:14 -0200

    full_title function that returns the title of the formated title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Function that returns the title of a page
+  def full_title(page_title)
+    base_title = "RailsNetwork"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
