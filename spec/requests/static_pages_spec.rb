42c5d188db484e6386c7c048bac1b4551e16a69a
Vitor Casadei <vitor.casadei@gmail.com>
2012-12-27 11:42:23 -0200

    Pages Layout and Tests updated

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,51 +2,49 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
     it "should have the h1 'Welcome to RailsNetwork'" do
-      visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Welcome to RailsNetwork')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
       page.should have_selector('title',
                         :text => "RailsNetwork")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
       page.should_not have_selector('title', :text => '| Home')
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
       page.should have_selector('title',
                         :text => "Help | RailsNetwork")
     end
   end
 
   describe "About page" do
+    before { visit about_path }
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
       page.should have_selector('title',
                     :text => "About Us | RailsNetwork")
     end
   end
+
 end
