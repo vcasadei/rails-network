18025040e1ca54a7db6bba8388f870104c53178f
Vitor Casadei <vitor.casadei@gmail.com>
2012-12-26 22:31:36 -0200

    First Commit - Project Creation

---
+++ README.md
@@ -0,0 +1,3 @@
+Rails Network - The Rails Social Network
+
+By Vitor Casadei
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsNetwork::Application.config.secret_token = '559048a472dbf7f74758508d0da469782c403a7fd44c171d79821a19416d7654de35c143c47494e3c6a93fa8e250c8184c7d5dde5aad159d29961c7e5fdc3ff6'
