817367e3ba691782a4d1b96f54382e8be8d65400
Vitor Casadei <vitor.casadei@gmail.com>
2012-12-28 14:53:18 -0200

    User Relationships Done with tests and Personalizations

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,7 +1,14 @@
 namespace :db do
   desc "Fill database with sample data"
   task populate: :environment do
-    admin = User.create!(name: "Example User",
+    make_users
+    make_posts
+    make_relationships
+  end
+end
+
+def make_users
+  admin = User.create!(name: "Example User",
                  email: "example@railstutorial.org",
                  cpf: "392.839.838-80",
                  gender: "male",
@@ -10,8 +17,7 @@ namespace :db do
                  password: "foobar",
                  password_confirmation: "foobar")
     admin.toggle!(:admin)
-
-    99.times do |n|
+  99.times do |n|
       name  = Faker::Name.name
       email = "example-#{n+1}@railstutorial.org"
       cpf = "392.839.838-80"
@@ -28,11 +34,22 @@ namespace :db do
                    password: password,
                    password_confirmation: password)
     end
+end
 
-    users = User.all(limit: 6)
-    50.times do
-      content = Faker::Lorem.sentence(10)
-      users.each { |user| user.posts.create!(content: content) }
-    end
+def make_posts
+  users = User.all(limit: 6)
+  50.times do
+    content = Faker::Lorem.sentence(10)
+    users.each { |user| user.posts.create!(content: content) }
   end
 end
+
+def make_relationships
+  users = User.all
+  user  = users.first
+  followed_users = users[2..50]
+  followers      = users[3..40]
+  followed_users.each { |followed| user.follow!(followed) }
+  followers.each      { |follower| follower.follow!(user) }
+end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,13 @@ describe User do
   it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
   it { should respond_to(:posts) } 
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
   it { should respond_to(:cpf) }
   it { should respond_to(:gender) }
   it { should respond_to(:city) }
@@ -137,27 +144,49 @@ describe User do
       FactoryGirl.create(:post, user: @user, created_at: 1.hour.ago)
     end
 
+
     describe "status" do
       let(:unfollowed_post) do
         FactoryGirl.create(:post, user: FactoryGirl.create(:user))
+      end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.posts.create!(content: "Lorem ipsum") }
       end
 
       its(:feed) { should include(newer_post) }
       its(:feed) { should include(older_post) }
       its(:feed) { should_not include(unfollowed_post) }
-    end
-
-    it "should have the right posts in the right order" do
-      @user.posts.should == [newer_post, older_post]
-    end
-
-    it "should destroy associated posts" do
-      posts = @user.posts.dup
-      @user.destroy
-      posts.should_not be_empty
-      posts.each do |post|
-        Post.find_by_id(post.id).should be_nil
+      its(:feed) do
+        followed_user.posts.each do |post|
+          should include(post)
+        end
       end
+    end
+  end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }    
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
     end
   end
 end
